1e53f423978c881795f7aa74d65e9037cc2fc39a
biran varon <biraniv@gmail.com>
2021-07-06 10:18:00 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -14,35 +14,9 @@
         </v-tab>
       </v-tabs>
 
-      <v-menu bottom rounded offset-y>
-        <template v-slot:activator="{ on }">
-          <v-btn icon v-on="on">
-            <v-avatar color="grey" size="32">
-              <span class="white--text">{{ user.initials }}</span>
-            </v-avatar>
-          </v-btn>
-        </template>
-        <v-card>
-          <v-list-item-content class="justify-center">
-            <div class="mx-auto text-center">
-              <h3>{{ user.fullName }}</h3>
-              <p class="text-caption mt-1">
-                {{ user.email }}
-              </p>
-              <v-divider class="my-3"></v-divider>
-              <v-btn
-                depressed
-                rounded
-                text
-                href="http://www.google.com"
-                target="_blank"
-              >
-                Download my CV!
-              </v-btn>
-            </div>
-          </v-list-item-content>
-        </v-card>
-      </v-menu>
+      <v-btn icon id="no-background-hover" large target="_blank" href="https://github.com/BiranV/mevn">
+        <v-icon medium>mdi-github</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main class="grey lighten-3">
