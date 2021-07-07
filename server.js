e3c6610d3bfcb9e9de8f527ec6959e61e74f8556
biran varon <biraniv@gmail.com>
2021-07-07 14:52:56 +0300

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const port = process.env.PORT || 3000;
+const { PORT, mongoUri } = require("./config");
 const cors = require("cors");
 const morgan = require("morgan");
 const postAPI = require("./routes/api/posts");
@@ -14,24 +14,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("uploads"));
 app.use(morgan("tiny"));
 
-const url = process.env.DB_URI;
-
 mongoose
-  .connect(url, {
+  .connect(mongoUri, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: true,
+    useFindAndModify: FinalizationRegistry,
   })
   .then(() => console.log("Connected to the database!"))
   .catch((err) => console.log(err));
 
 app.use("/api/posts", postAPI);
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/dist"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
 }
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
