568729afffe58b39f1a0cdae30531ebf948b64cc
biran varon <biraniv@gmail.com>
2021-07-07 23:53:44 +0300

    update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: 3000,
+  PORT: process.end.PORT || 3000,
   MONGO_URI:
     "mongodb+srv://biran:uPbitKjvufjOsTBF@cluster0.hsipf.mongodb.net/test?retryWrites=true&w=majority",
 };
