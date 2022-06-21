8f954d0aa53a1c6cedd061b836a9d7ea5b298622
biran varon <biraniv@gmail.com>
2022-06-21 18:27:57 +0300

    update

--- server.js
+++ server.js
@@ -5,22 +5,22 @@ const cors = require("cors");
 const morgan = require("morgan");
 const postAPI = require("./routes/routes");
 const path = require("path");
-const history = require("connect-history-api-fallback"); // for the ability to refresh a page.
+//const history = require("connect-history-api-fallback"); // for the ability to refresh a page.
 require("dotenv").config();
 
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
-app.use(
-  history({
-    index: "",
-  })
-);
+// app.use(
+//   history({
+//     index: "/default.html",
+//   })
+// );
 
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
   })
@@ -36,4 +36,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname + "client", "dist", "index.html"));
   });
 }
-app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running on port ${process.env.PORT}`)
+);
