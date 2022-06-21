283858cfd2c0b2d4e2e7281bbb85180047625d5c
biran varon <biraniv@gmail.com>
2022-06-21 19:03:45 +0300

    update

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.use(
 
 mongoose
   .connect(process.env.MONGO_URI, {
-    // useNewUrlParser: true,
-    // useCreateIndex: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
   })
